b73096ca052175f4c2563be565b4a517e55a28bf
tdarbeloff <tracydarbeloff@gmail.com>
2018-06-08 15:45:59 -0400

    checked over/transformed adult variables

--- CogVar_Descrps.R
+++ CogVar_Descrps.R
@@ -13,6 +13,7 @@ library(investr)
 library(MASS)
 library(lmSupport)
 library(caret)
+library(broom)
 
 #Read in Datasets
 
@@ -55,5 +56,59 @@ d_gwas <- d_gwas[c(17, 2, 14)]
 d_gwasNA <- na.omit(d_gwas)
 
 
+#combine datasets by ID
+d_1 <- inner_join(d_adltNA, d_chldNA, by=c("ID"))
+dcog <- inner_join(d_1, d_gwasNA, by=c("ID"))
 
-#Look over Cog variables and transform
+
+
+#Look over Cog variables and transform/z-score
+
+hist(dcog$vci45a)
+vci45_desc <- describe(dcog$vci45a)
+#Verbal comprehension at age 45 is normally distributed
+
+hist(dcog$pri45a)
+pri45_desc <- describe(dcog$pri45a)
+#perecptual reasoning age 45 is relatively normally distributed
+
+hist(dcog$wmi45a)
+wmi_desc <- describe(dcog$wmi45a)
+#working memory index age 45 is relatively normally distributed
+
+hist(dcog$psi45a)
+psi_desc <- describe(dcog$psi45a)
+#normal enough
+
+hist(dcog$fsiq45a)
+fsiq_desc <- describe(dcog$fsiq45a)
+#normal enough
+
+hist(dcog$GpDomTim45)
+GpDom45_desc <- describe(dcog$GpDomTim45)
+dcog$GpDomTim45_lg <- log(dcog$GpDomTim45)
+boxplot(dcog$GpDomTim45_lg)
+#log transformed, looks like there are some potential 
+#outliers that may come up in future analyses
+
+hist(dcog$GpDif45)
+GpDif45_desc <- describe(dcog$GpDif45) 
+boxplot(dcog$GpDif45)
+#some clear outliers, unsure whether to transform or 
+#deal with that at all right now...
+
+hist(dcog$RAVLtot45)
+RAVLtot45_desc <- describe(dcog$RAVLtot45)
+#normal
+
+hist(dcog$RAVLrec45)
+RAVLrec45_desc <- describe(dcog$RAVLrec45)
+#normal enough
+
+hist(dcog$TrailB45)
+TrailB45_desc <- describe(dcog$TrailB45)
+dcog$TrailB45_sqrt <- sqrt(dcog$TrailB45)
+boxplot(dcog$TrailB45_sqrt)
+hist(dcog$TrailB45_sqrt)
+#not sure if we want to stay with that transformation. Something to think about.
+
